71ae816a9ffce7f5671dd918aa9e16db2d6a5d38
sakod <vitor.mendes.ag@gmail.com>
2024-02-27 14:38:37 -0300

    baralho em forma dicionario

--- poker_simulator.py
+++ poker_simulator.py
@@ -2,21 +2,25 @@ import random
 
 class Baralho:
     def __init__(self): # baralho com cartas do poker
-        self.cartas = ['A_e', 'A_c', 'A_t', 'A_o', 'K_e', 'K_c', 'K_t', 'K_o',
-                       'Q_e', 'Q_c', 'Q_t', 'Q_o', 'J_e', 'J_c', 'J_t', 'J_o',
-                       '10_e', '10_c', '10_t', '10_o', '9_e', '9_c', '9_t', '9_o',
-                       '8_e', '8_c', '8_t', '8_o', '7_e', '7_c', '7_t', '7_o',
-                       '6_e', '6_c', '6_t', '6_o', '5_e', '5_c', '5_t', '5_o',
-                       '4_e', '4_c', '4_t', '4_o', '3_e', '3_c', '3_t', '3_o',
-                       '2_e', '2_c', '2_t', '2_o']
+        self.cartas = {'o':{14:'A_o',13:'K_o',12:'Q_o',11:'J_o',10:'10_o',9:'9_o',8:'8_o',7:'7_o',6:'6_o',5:'5_o',4:'4_o',3:'3_o',2:'2_o'},
+                       't':{14:'A_t',13:'K_t',12:'Q_t',11:'J_t',10:'10_t',9:'9_t',8:'8_t',7:'7_t',6:'6_t',5:'5_t',4:'4_t',3:'3_t',2:'2_t'},
+                       'e':{14:'A_e',13:'K_e',12:'Q_e',11:'J_e',10:'10_e',9:'9_e',8:'8_e',7:'7_e',6:'6_e',5:'5_e',4:'4_e',3:'3_e',2:'2_e'},
+                       'c':{14:'A_c',13:'K_c',12:'Q_c',11:'J_c',10:'10_c',9:'9_c',8:'8_c',7:'7_c',6:'6_c',5:'5_c',4:'4_c',3:'3_c',2:'2_c'}}
         self.embaralhar()
 
-    def embaralhar(self): # embaralhando
-        random.shuffle(self.cartas)
+    def embaralhar(self) -> list: # embaralhando
+        baralho_dealer = []
+        for naipe, cards in self.cartas.items():
+            for carta in cards.values():
+                baralho_dealer.append(carta)
+        random.shuffle(baralho_dealer)
+        return baralho_dealer
+                
+        #random.shuffle(self.cartas)
 
 
-    def distribuir_cartas(self, num_jogadores, cartas_por_jogador): # distribuindo cartas
-        if num_jogadores * cartas_por_jogador > len(self.cartas): # teste para ver se tem mais jogadores que cartas
+    def distribuir_cartas(self, num_jogadores:int, cartas_por_jogador:int, baralho_dealer:list)->dict: # distribuindo cartas para os jogadores
+        if num_jogadores * cartas_por_jogador > len(baralho_dealer): # teste para ver se tem mais jogadores que cartas
             print("Não há cartas suficientes para distribuir.")
             return {}
 
@@ -24,14 +28,15 @@ class Baralho:
         for jogador in range(1, num_jogadores + 1):
             mao = []
             for _ in range(cartas_por_jogador):
-                carta = self.cartas.pop(0)
+                carta = baralho_dealer.pop(0)
                 mao.append(carta)
             maos[f"Jogador {jogador}"] = mao
         return maos
 
 
-    def distribuir_cartas_flop(self):
-        embaralhadas = list(self.cartas)
+
+    def distribuir_cartas_flop(self,embaralhado):
+        embaralhadas = embaralhado
         flop = []
         turn = []
         river = []
@@ -40,24 +45,38 @@ class Baralho:
         river = embaralhadas[8]
         return flop, turn, river
 
-    def quantificando_baralho(self):
+    def quantificando_baralho(self, maos):
         carta_split = []
-        for carta in self.cartas:
-            carta_split.append(carta.split('_'))
+        num_card = []
+        naipe_card = []
+        for jogador, cartas in maos.items():
+            for carta in cartas:
+                carta_split = carta.split('_')
+               
+               
+                
+
+        print(num_card)
         #if carta_split[[0]] is  # stop here
 
+#class poker_texas_holdem:
+
+#    def __init__(self) -> None:
+#        pass
 
 # Exemplo de uso:
 
 baralho = Baralho()
+# configuração de players e modalidade do jogo
 num_jogadores = 6
 cartas_por_jogador = 2
-baralho.quantificando_baralho()
-maos = baralho.distribuir_cartas(num_jogadores, cartas_por_jogador)
-print(maos)
 
+# embraralhar e distribuir cartas dos jogadores e flop
+embaralhado = baralho.embaralhar()
 
-flop, turn, river = baralho.distribuir_cartas_flop()
+maos = baralho.distribuir_cartas(num_jogadores, cartas_por_jogador, embaralhado)
+
+flop, turn, river = baralho.distribuir_cartas_flop(embaralhado)
 print(f'flop: {flop}')
 print(f'turn: {turn}')
 print(f'river: {river}')
